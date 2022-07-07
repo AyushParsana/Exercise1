c52541bd54225b2c734a03c65bdc0a37fbaa6ec6
Ayush Parsana <tobe4627@gmail.com>
2022-07-07 10:41:12 +0530

    0701

---
+++ 1/1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello!");
+            Console.WriteLine("Aayush Parashana");
+        }
+    }
+}
