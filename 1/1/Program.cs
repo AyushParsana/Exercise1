0580b540c8ab803a9cd57b3faa9c13b3c4d26cd3
AyushParsana <108853453+AyushParsana@users.noreply.github.com>
2022-07-07 10:44:43 +0530

    Update Program.cs

--- 1/1/Program.cs
+++ 1/1/Program.cs
@@ -9,7 +9,7 @@ namespace _1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello!");
+            Console.WriteLine("Hello World!");
             Console.WriteLine("Aayush Parashana");
         }
     }
